0fd8b68f66f96d82c1091302def999bdc78623bc
LourdesAnne <42105712+LourdesAnne@users.noreply.github.com>
2018-08-05 15:47:35 +0800

    Add files via upload

---
+++ Francisco_Programming.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+
+    cout<<("L.A 's Restaurant\n\n");
+    cout<<("L.A. 's Menu \n \n");
+
+    cout<< "Meals \n";
+
+    int KrispyPata = 120;
+    int Calamares = 110;
+    int Chicken = 90;
+    int ButterShrimp = 140;
+    int BuffaloWings = 155;
+    int ExtraRice = 10;
+    
+    cout <<endl<< "KrispyPata = " << KrispyPata;
+    cout <<endl<< "Calamares = " << Calamares;
+    cout <<endl<< "Chicken = " << Chicken;
+    cout <<endl<< "ButterShrimp = " << ButterShrimp;
+    cout <<endl<< "BuffaloWings = " <<BuffaloWings;
+    cout <<endl<< "ExtraRice = " <<ExtraRice;
+    cout << endl << endl <<"Drinks \n";
+    
+    int FruitJuice = 20;
+    int FruitShake  = 30;
+    int MilkShake = 40;
+    int SoftDrinks = 25;
+    int BottledWater = 20;
+
+    cout <<endl<< "FruitJuice = " <<FruitJuice;
+    cout <<endl<< "FruitShake = " <<FruitShake;
+    cout <<endl<< "MilkShake = " <<MilkShake;
+    cout <<endl<< "SoftDrinks = " <<SoftDrinks;
+    cout <<endl<< "BottledWater = " <<BottledWater;
+
+    cout << endl << endl << "Desserts\n";
+
+    int BlueberryCake = 85;
+    int GrahamCake = 60;
+    int BananaSplit = 70;
+    int ApplePie = 70;
+    int MashedPotato = 50;
+
+    cout <<endl << "BlueberryCake = " <<BlueberryCake;
+    cout <<endl << "GrahamCake = " << GrahamCake;
+    cout <<endl << "BananaSplit = " <<BananaSplit;
+    cout <<endl << "ApplePie = " <<ApplePie;
+    cout <<endl << "MashedPotato =" <<MashedPotato;
+
+  
+    return 0;
+}
